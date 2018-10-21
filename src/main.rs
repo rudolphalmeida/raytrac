95b6c87da6f88792e61c657916dd8386591d4dde
Rudolph Almeida <rudolf1.almeida@gmail.com>
2018-10-21 10:53:59 +0530

    Removed constant spheres from loop which greatly increased render time

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ use std::sync::Arc;
 fn main() {
     const WIDTH: u16 = 350;
     const HEIGHT: u16 = 200;
-    const SAMPLES: u64 = 50;
+    const SAMPLES: u64 = 500;
 
     let camera = Camera::new(
         Point3::new(13.0, 2.0, 3.0),
@@ -49,88 +49,85 @@ fn main() {
 }
 
 fn random_scene() -> HittableList {
-    let mut rng = rand::thread_rng();
+    // let mut rng = rand::thread_rng();
 
     let mut list: HittableList = HittableList::new();
     list.add(Box::new(Sphere::from(
         Point3::new(0.0, -1000.0, 0.0),
         1000.0,
         Arc::new(Material::Lambertian(Lambertian::new(
-            Texture::CheckedTexture(CheckedTexture::new(
-                Texture::ConstantTexture(ConstantTexture::from(0.5, 0.5, 0.5)),
-                Texture::ConstantTexture(ConstantTexture::from(0.2, 0.6, 0.4)),
-            )),
+            Texture::ConstantTexture(ConstantTexture::from(0.5, 0.3, 0.8)),
         ))),
     )));
 
-    for a in -11..11 {
-        for b in -11..11 {
-            let choose_mat = rng.gen::<f64>();
-            let center = Point3::new(
-                f64::from(a) + 0.9 * rng.gen::<f64>(),
-                0.2,
-                f64::from(b) + 0.9 * rng.gen::<f64>(),
-            );
-            if (center - Vector3::new(4.0, 0.2, 0.0)).to_vec().magnitude() > 0.9 {
-                if choose_mat < 0.8 {
-                    // diffuse
-                    list.add(Box::new(Sphere::from(
-                        center,
-                        0.2,
-                        Arc::new(Material::Lambertian(Lambertian::new(
-                            Texture::ConstantTexture(ConstantTexture::from(
-                                rng.gen::<f64>() * rng.gen::<f64>(),
-                                rng.gen::<f64>() * rng.gen::<f64>(),
-                                rng.gen::<f64>() * rng.gen::<f64>(),
-                            )),
-                        ))),
-                    )));
-                } else if choose_mat < 0.95 {
-                    //metal
-                    list.add(Box::new(Sphere::from(
-                        center,
-                        0.2,
-                        Arc::new(Material::Metal(Metal::new(
-                            Vector3::new(
-                                0.5 * (1.0 + rng.gen::<f64>()),
-                                0.5 * (1.0 + rng.gen::<f64>()),
-                                0.5 * (1.0 + rng.gen::<f64>()),
-                            ),
-                            0.5 * rng.gen::<f64>(),
-                        ))),
-                    )));
-                } else {
-                    // dielectric
-                    list.add(Box::new(Sphere::from(
-                        center,
-                        0.2,
-                        Arc::new(Material::Dielectric(Dielectric::from(1.5))),
-                    )));
-                }
-            }
+    // for a in -11..11 {
+    //     for b in -11..11 {
+    //         let choose_mat = rng.gen::<f64>();
+    //         let center = Point3::new(
+    //             f64::from(a) + 0.9 * rng.gen::<f64>(),
+    //             0.2,
+    //             f64::from(b) + 0.9 * rng.gen::<f64>(),
+    //         );
+    //         if (center - Vector3::new(4.0, 0.2, 0.0)).to_vec().magnitude() > 0.9 {
+    //             if choose_mat < 0.8 {
+    //                 // diffuse
+    //                 list.add(Box::new(Sphere::from(
+    //                     center,
+    //                     0.2,
+    //                     Arc::new(Material::Lambertian(Lambertian::new(
+    //                         Texture::ConstantTexture(ConstantTexture::from(
+    //                             rng.gen::<f64>() * rng.gen::<f64>(),
+    //                             rng.gen::<f64>() * rng.gen::<f64>(),
+    //                             rng.gen::<f64>() * rng.gen::<f64>(),
+    //                         )),
+    //                     ))),
+    //                 )));
+    //             } else if choose_mat < 0.95 {
+    //                 //metal
+    //                 list.add(Box::new(Sphere::from(
+    //                     center,
+    //                     0.2,
+    //                     Arc::new(Material::Metal(Metal::new(
+    //                         Vector3::new(
+    //                             0.5 * (1.0 + rng.gen::<f64>()),
+    //                             0.5 * (1.0 + rng.gen::<f64>()),
+    //                             0.5 * (1.0 + rng.gen::<f64>()),
+    //                         ),
+    //                         0.5 * rng.gen::<f64>(),
+    //                     ))),
+    //                 )));
+    //             } else {
+    //                 // dielectric
+    //                 list.add(Box::new(Sphere::from(
+    //                     center,
+    //                     0.2,
+    //                     Arc::new(Material::Dielectric(Dielectric::from(1.5))),
+    //                 )));
+    //             }
+    //         }
+    //     }
+    // }
 
-            list.add(Box::new(Sphere::from(
-                Point3::new(0.0, 1.0, 0.0),
-                1.0,
-                Arc::new(Material::Dielectric(Dielectric::from(1.5))),
-            )));
-            list.add(Box::new(Sphere::from(
-                Point3::new(-4.0, 1.0, 0.0),
-                1.0,
-                Arc::new(Material::Lambertian(Lambertian::new(
-                    Texture::ConstantTexture(ConstantTexture::from(0.4, 0.4, 0.1)),
-                ))),
-            )));
-            list.add(Box::new(Sphere::from(
-                Point3::new(4.0, 1.0, 0.0),
-                1.0,
-                Arc::new(Material::Metal(Metal::new(
-                    Vector3::new(0.7, 0.6, 0.5),
-                    0.0,
-                ))),
-            )));
-        }
-    }
+    list.add(Box::new(Sphere::from(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        Arc::new(Material::Dielectric(Dielectric::from(1.5))),
+    )));
+    list.add(Box::new(Sphere::from(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        Arc::new(Material::Lambertian(Lambertian::new(
+            Texture::ConstantTexture(ConstantTexture::from(0.4, 0.4, 0.1)),
+        ))),
+    )));
+    list.add(Box::new(Sphere::from(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        Arc::new(Material::Metal(Metal::new(
+            Vector3::new(0.7, 0.6, 0.5),
+            0.0,
+        ))),
+    )));
 
     list
 }
